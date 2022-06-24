a87a6e3227329d92c105a3cc1009adfd2b3ba85c
pablorodrigo <pablo.silva.rodrigo@gmail.com>
2022-06-24 16:22:52 -0400

    add my projects details

--- src/assets/js/my-scripts.js
+++ src/assets/js/my-scripts.js
@@ -1,6 +1,7 @@
 $(function(){
     $("#include-profile").load("src/view/profile.html");
     $("#include-projects").load("src/view/projects.html");
+    $("#include-modals-projects").load("src/view/modals-projects.html");
 
 
 });
