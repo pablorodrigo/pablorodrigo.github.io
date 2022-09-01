8c2bccee19dba6ed29ed7a691880d3b00b7c08a0
pablorodrigo <pablo.silva.rodrigo@gmail.com>
2022-08-31 22:39:06 -0400

    created new section called howToDev

--- src/assets/js/my-scripts.js
+++ src/assets/js/my-scripts.js
@@ -2,6 +2,7 @@ $(function(){
     $("#include-profile").load("src/view/profile.html");
     $("#include-projects").load("src/view/projects.html");
     $("#include-modals-projects").load("src/view/modals-projects.html");
+    $("#include-howtodev").load("src/view/howtodev.html");
     $("#include-publications").load("src/view/publications.html");
     $("#include-modals-publications").load("src/view/modals-publications.html");
 });
